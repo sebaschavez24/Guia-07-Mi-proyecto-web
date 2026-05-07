0429e60aede010e57b4fb2174fd588f4b9f87b1e
Bryan Chavez <215817@unsaac.edu.pe>
2026-05-06 23:11:33 -0500

    Se agrega formulario de contacto

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function saludar() {
         "Hola " + nombre + ", bienvenido al sistema.";
     }
 }
+function validarCorreo() {
+    let correo = document.getElementById("correo").value;
+    if (correo === "") {
+        document.getElementById("mensajeCorreo").innerText = "Debe ingresar un correo.";
+    } else {
+    document.getElementById("mensajeCorreo").innerText = "Correo  registrado correctamente.";
+}
+}
